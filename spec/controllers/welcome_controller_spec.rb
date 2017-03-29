8b7a330d753f250e7f4d88d6364e8190bb110e85
Steven <syhoung1@gmail.com>
2017-03-29 16:43:29 +0000

    added test for FAQ

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe WelcomeController, type: :controller do
     describe "GET index" do 
         it "renders the index template" do
             get :index
-
             expect(response).to render_template("index")
         end
     end
@@ -12,8 +11,14 @@ RSpec.describe WelcomeController, type: :controller do
     describe "GET about" do
         it "renders the about template" do
             get :about
-
             expect(response).to render_template("about")
+        end
+    end
+
+    describe "GET FAQ" do
+        it "renders the FAQ template" do
+            get :FAQ
+            expect(response).to render_template("FAQ")
         end
     end
 end

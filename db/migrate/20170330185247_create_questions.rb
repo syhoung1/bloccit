8e7d33406b1b41d28711c98e0ef0cf53a7d8a968
Steven <syhoung1@gmail.com>
2017-03-30 19:20:43 +0000

    has many answers

--- db/migrate/20170330185247_create_questions.rb
+++ db/migrate/20170330185247_create_questions.rb
@@ -4,7 +4,8 @@ class CreateQuestions < ActiveRecord::Migration
       t.string :title
       t.text :body
       t.boolean :resolved
-
+      t.has_many :answers
+      
       t.timestamps null: false
     end
   end

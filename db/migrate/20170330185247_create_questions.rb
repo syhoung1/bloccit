53e1d1f9b00f871d5ebbeb27897e12ad765a2763
Steven <syhoung1@gmail.com>
2017-03-31 04:28:01 +0000

    actually moved has_many

--- db/migrate/20170330185247_create_questions.rb
+++ db/migrate/20170330185247_create_questions.rb
@@ -4,8 +4,7 @@ class CreateQuestions < ActiveRecord::Migration
       t.string :title
       t.text :body
       t.boolean :resolved
-      t.has_many :answers
-      
+
       t.timestamps null: false
     end
   end

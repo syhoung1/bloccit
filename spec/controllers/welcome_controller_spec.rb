c153cc89f51357b81ae52e22bd795148badf9fa9
Steven <syhoung1@gmail.com>
2017-03-31 15:31:19 +0000

    pry rails

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-    describe "GET index" do 
-        it "renders the index template" do
-            get :index
-            expect(response).to render_template("index")
-        end
-    end
+  describe "GET index" do 
+    it "renders the index template" do
+      get :index
+    expect(response).to render_template("index")
+  end
+end
 
-    describe "GET about" do
-        it "renders the about template" do
-            get :about
-            expect(response).to render_template("about")
-        end
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
     end
+  end
 
-    describe "GET FAQ" do
-        it "renders the FAQ template" do
-            get :FAQ
-            expect(response).to render_template("FAQ")
-        end
+  describe "GET FAQ" do
+    it "renders the FAQ template" do
+      get :FAQ
+      expect(response).to render_template("FAQ")
     end
+  end
 end

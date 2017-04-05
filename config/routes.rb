1a4ccbb267116200db5826124b89f8b450e1fffa
Steven <syhoung1@gmail.com>
2017-04-05 18:05:46 +0000

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
+    resources :users, only: [:new, :create]
   end
 
   get 'about' => 'welcome#about'

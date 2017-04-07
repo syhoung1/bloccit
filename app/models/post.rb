22ca959b9d52fdf3356873940688fada4b711c69
Steven <syhoung1@gmail.com>
2017-04-07 05:45:08 +0000

    more scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('title DESC') } 
+  scope :ordered_by_created_at_reverse, -> { order('created_at ASC') }
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
